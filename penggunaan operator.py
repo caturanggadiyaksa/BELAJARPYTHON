57ac1358ddaeb924f87e4e62cbd23fc5b2c760ab
Catur Angga Adiyaksa <88980174+caturanggadiyaksa@users.noreply.github.com>
2022-02-08 10:52:42 +0700

    contoh penggunaan operatro pada python
    contoh penggunaan operator pada bahasa pemograman python

---
+++ penggunaan operator.py
@@ -0,0 +1,5 @@
+print ("2 + 5 sama dengan ", 2 + 5) #penggunaan operator + penjumlahan
+print ("4 * 4 sama dengan", 4 * 4)  #penggunaan operator * perkalian
+print ("100-101 sama dengan", 100 - 101)  #penggunaan operator - pengurangan
+print ("40/2 + 10*4 sama dengan"  (40/2)+(10*4))  #penggunaan operator / pembagian dan operatro () untuk mengutamakan 
+#ini komen
