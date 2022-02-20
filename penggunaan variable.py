cce7822c72e4670eaa31e6895ed21705bc9cbe79
Catur Angga Adiyaksa <88980174+caturanggadiyaksa@users.noreply.github.com>
2022-02-20 17:18:53 +0700

    contoh penggunaan variable dalam python

---
+++ penggunaan variable.py
@@ -0,0 +1,9 @@
+angka1 = 678.8
+angka2 = 13
+nama_depan = "Catur"
+katamisteri = "adi"
+jumlah = angka1 + angka2
+
+print ("hasil penjumlahan 678.8 + 13 =", jumlah)
+print ("nama depan pak Catur adalah ", nama_depan)
+print ("kakaknya angga adalah", katamisteri)
